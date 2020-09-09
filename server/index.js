7b7d8f5b8c7c85fd2708d4e6278fd4a8c30eefe2
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-10 03:14:45 +0530

    Mongoose Setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
+// usR3YQfaRoQEfQ3Q
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 require("./routes/authRoutes")(app);
