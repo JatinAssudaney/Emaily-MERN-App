f926fa74dcf5f9cc44eb4d2d42a6cebe260eded4
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-10 14:54:01 +0530

    PassportJS Google OAuth using cookie-session

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +22,16 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("access Token", accessToken);
-      console.log("refresh Token", refreshToken);
-      console.log("profile:", profile);
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // We already have a record with the given profile ID
+        done(null, existingUser);
+      } else {
+        // We don't have a user record with this ID, make a new record
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
