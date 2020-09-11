ae6d8a0ee401279f51ab35b8e1d0ce028e793460
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-12 00:44:09 +0530

    OAuth flow complete React-Node

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const App = () => {
-  return <div>"Hi There"</div>;
+import Header from "./Header";
+import Landing from "./Landing";
+// import Dashboard from "./Dashboard";
+// import SurveyNew from "./surveys/SurveyNew";
+import { fetchUser } from "../actions";
+
+const NoMatchPage = () => {
+  return <h2>404 NOT FOUND</h2>;
+};
+const Dashboard = () => {
+  return <h2>Dashboard</h2>;
+};
+const SurveyNew = () => {
+  return <h2>Survey New</h2>;
 };
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/surveys" component={Dashboard} />
+              <Route exact path="/surveys/new" component={SurveyNew} />
+              <Route component={NoMatchPage} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = { fetchUser };
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,5 +1,10 @@
-export default (state = {}, action) => {
-  switch (action.type) {
+import { FETCH_USER } from "../actions/types";
+const initialState = null;
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case FETCH_USER:
+      return payload || false;
     default:
       return state;
   }
